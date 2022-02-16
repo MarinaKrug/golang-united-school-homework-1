f97513aec5a0a9d851a898a03727e2305ad86605
krugl <marinka22@inbox.ru>
2022-02-16 10:21:22 +0300

    done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module m/v0
+module solution.go
 
 go 1.17
+
+require github.com/kyokomi/emoji v2.2.4+incompatible
--- solution.go
+++ solution.go
@@ -1,9 +1,12 @@
-package solution
+package main
 
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji"
 )
 
-func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:!")
+func main() {
+	helloWorld := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(helloWorld)
 }
